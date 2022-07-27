1f898aa764abca369b66baff72837cd1c94c9db9
Jake Weilhammer <jake.weilhammer@gmail.com>
2022-07-27 16:05:15 -0400

    Make tiles draggable! :-)

--- driver.js
+++ driver.js
@@ -645,4 +645,57 @@ const solvePuzzleForFunzies = async (htmlMatrix, puzzle, goal_state) => {
     }
 }
 
-solvePuzzleForFunzies(htmlMatrix, new Puzzle(genRandomPuzzle=true), goal_state);
+// solvePuzzleForFunzies(htmlMatrix, new Puzzle(genRandomPuzzle=true), goal_state);
+
+// https://web.dev/drag-and-drop/
+document.addEventListener('DOMContentLoaded', (e) => {
+
+    function handleDragStart(e) {
+      this.style.opacity = '0.4';
+
+      dragSrcEl = this;
+
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/html', this.innerHTML);
+    }
+  
+    function handleDragEnd(e) {
+      this.style.opacity = '1';
+  
+      items.forEach(function (item) {
+        item.classList.remove('over');
+      });
+    }
+  
+    function handleDragOver(e) {
+      e.preventDefault();
+      return false;
+    }
+  
+    function handleDragEnter(e) {
+      this.classList.add('over');
+    }
+  
+    function handleDragLeave(e) {
+      this.classList.remove('over');
+    }
+
+    function handleDrop(e) {
+        if (dragSrcEl !== this) {
+            dragSrcEl.innerHTML = this.innerHTML;
+            this.innerHTML = e.dataTransfer.getData('text/html');
+          }
+        
+        return false;
+    }
+  
+    let items = document.querySelectorAll('.grid-item');
+    items.forEach(function(item) {
+      item.addEventListener('dragstart', handleDragStart);
+      item.addEventListener('dragover', handleDragOver);
+      item.addEventListener('dragenter', handleDragEnter);
+      item.addEventListener('dragleave', handleDragLeave);
+      item.addEventListener('dragend', handleDragEnd);
+      item.addEventListener('drop', handleDrop);
+    });
+  });
